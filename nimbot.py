c46833379f7665a75275c47cb2410c3f27bd8929
whiteosf <olkabuglak@gmail.com>
2020-02-13 16:33:49 +0000

    changed, not commented

--- nimbot.py
+++ nimbot.py
@@ -13,6 +13,5 @@ async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
 
 bot.run(token)
-#Commenting:   See you did a lot. 
-# Btw, what does .gitignore contain and is it used in the actual bot work, or just acts as info?
 
+#changed
